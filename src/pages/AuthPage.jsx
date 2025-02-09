65c38e1e75f29d57f30ce3918c62b53b0c394455
37anton <antonin.cavanne@outlook.fr>
2025-02-09 15:21:53 +0100

    installation tailwind + axios + notyf

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import AuthForm from '../components/AuthForm/AuthForm';
+import notyf from "../utils/notyf";
 
 const AuthPage = () => {
   return (
-    <div className="auth-page">
+    // bg-red-400 pour tester tailwind
+    <div className="auth-page bg-red-400"> 
       <h1>Bienvenue sur l'application Chifoumi</h1>
       <AuthForm />
     </div>
   );
 };
+
+// Mise en place de la notyf uniquement pour tester l'installation
+notyf.success("Bienvenue sur l'application.");
 
 export default AuthPage;
